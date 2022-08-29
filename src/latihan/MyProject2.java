9aa9fa40b42b56d5be9a1702c519ebf4e106ea2c
agyhafidzah <you@example.com>
2022-08-29 22:28:23 +0700

    project pertama

---
+++ src/latihan/MyProject2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author ACER
+ */
+public class MyProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("      A          GGGGGGG   YY     YY");
+    System.out.println("     AAA        GG          YY   YY ");
+    System.out.println("    AA AA      GGG           YY YY  ");
+    System.out.println("   AA   AA     GGG            YYY   ");
+    System.out.println("  AAAAAAAAA    GGG   GGGG     YYY   ");
+    System.out.println(" AA       AA    GG    GGG     YYY   ");
+    System.out.println("AA         AA    GGGGGGG      YYY   ");
+    }
+    
+}
